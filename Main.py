51210ccb1f0db48ad8b073efbe3468792885b257
Benjamin Fuller <benisfull@gmail.com>
2022-08-25 14:47:52 -0700

    pep8 changes

--- Main.py
+++ Main.py
@@ -1,13 +1,16 @@
 from Riot_API import RiotAPI
 
+
 def main():
-    api = RiotAPI('') #('Put your own riot api key here')
+    api = RiotAPI('')  # ('Put your own riot api key here')
     sum_name = input('Please enter summoner name:')
     sumVal = api.get_summoner_by_name(sum_name)
-    print (f"Here is the profile of the summoner you requested:\n{sumVal}")
+    print(f"Here is the profile of the summoner you requested:\n{sumVal}")
     sumMatch = api.get_summoner_matches(sumVal['puuid'], 0, 5)
-    print(f"Here are the ids of the last 5 matches this summoner has participated in:\n{sumMatch}\n") 
+    print(f"Here are the ids of the last 5 matches this summoner has participated in:\n{sumMatch}\n")
     matchP = api.get_match_details(sumMatch[0])
     print(matchP)
+
+
 if __name__ == "__main__":
-    main()      
+    main()
--- Riot_API.py
+++ Riot_API.py
@@ -1,12 +1,15 @@
 import requests
 import RiotConsts as Consts
 
+
 class RiotAPI(object):
-    def __init__(self, api_key): #Contructor that sets the users api_key to be used for the requests, can be found in Main.py
+    def __init__(self,
+                 api_key):  # Contructor that sets the users api_key to be used for the requests, found in # Main.py
         self.api_key = api_key
         # self.region = region
 
-    def requests(self, api_url, params={}): #function that takes url parameters set by the get_functions and makes a request 
+    def requests(self, api_url,
+                 params={}):  # function that takes url parameters set by the get_functions and makes a request
         args = {'api_key': self.api_key}
         for key, value in params.items():
             if key not in args:
@@ -15,7 +18,7 @@ class RiotAPI(object):
         print(f"The request url you are using is:\n{response.url}\n")
         return response.json()
 
-    def get_summoner_by_name(self, name): #Gets the summoners data using the summoner name
+    def get_summoner_by_name(self, name):  # Gets the summoners data using the summoner name
         api_url = Consts.URL['summoner_by_name'].format(
             proxy=Consts.REGIONS['north_america_1'],
             version=Consts.API_VERSIONS['summoner'],
@@ -23,7 +26,7 @@ class RiotAPI(object):
         )
         return self.requests(api_url)
 
-    #Does essentially the same thing as get_summoner_name()
+    # Does essentially the same thing as get_summoner_name()
     # def get_tft_summoner_name(self, name):
     #     api_url = Consts.URL['tft_summoner_by_name'].format(
     #      version=Consts.API_VERSIONS['tft_summoner'],
@@ -31,7 +34,8 @@ class RiotAPI(object):
     #     )
     #     return self.requests(api_url)
 
-    def get_summoner_matches(self, puuid, startNum, matchQuant): #Gets the summoners match history with their puuid, can specify how many matches to get
+    def get_summoner_matches(self, puuid, startNum,
+                             matchQuant):  # Gets the summoners match history, can specify how many matches
         api_url = Consts.URL['summoner_matches_by_puuid'].format(
             proxy=Consts.REGIONS['america'],
             version=Consts.API_VERSIONS['summoner_matches'],
@@ -41,7 +45,7 @@ class RiotAPI(object):
         )
         return self.requests(api_url)
 
-    def get_match_details(self, matchID): #Gets match details with a given match id
+    def get_match_details(self, matchID):  # Gets match details with a given match id
         api_url = Consts.URL['match_by_match_ID'].format(
             proxy=Consts.REGIONS['america'],
             version=Consts.API_VERSIONS['match'],
