d05e9403ad34c0df2bb3f030574c744017d20a10
Kyle Woo <ktw1143@gmail.com>
2022-10-24 22:52:18 -0700

    Deleted commented code added more info to be returned by team_Kills_Compare
    Added least kills, and average kills for team_Kills compare function and a corresponding output in the main file to display those stats. Also deleted code that is no longer needed

--- lolAPI.py
+++ lolAPI.py
@@ -1,4 +1,4 @@
-from riotwatcher import LolWatcher
+from riotwatcher import LolWatcher                                        
 
 class lol_Comp:
   
@@ -7,79 +7,93 @@ class lol_Comp:
     self.region = region
     self.team = team
     self.team_List = []
-    self.sum_Prof = LolWatcher(self.api_key, default_status_v4=True)
+    self.watcher = LolWatcher(self.api_key, default_status_v4=True)
 
-    # self.team_List = [self.team_List.append(sum_Prof.summoner.by_name(self.region, self.team[i])) for i in range(0,5)]
-    
-    # for i in range(0, 5):
-    #   self.team_List.append(sum_Prof.summoner.by_name(self.region, self.team[i]))
-
-  def create_Team(self):
+  def create_Team(self): 
+    # Creates a list of lolwatcher objects using the list of team names given by user.
     for i in range(len(self.team)):
-      self.team_List.append(self.sum_Prof.summoner.by_name(self.region, self.team[i]))
-    print(type(self.team_List[0]))
-    print(self.team_List[0]['name'])
-
+      self.team_List.append(self.watcher.summoner.by_name(self.region, self.team[i]))
+    
   def get_Team_Info(self):
-    final_Info_List = []
-    for i in range(0, len(self.team)):
-      team_List_Info = []
-      team_List_Info.append(self.team_List[i])
-      final_Info_List.append(team_List_Info)
-    return final_Info_List
-                            
-    return team_List_Info
+    # Used to format and return summoner's profile data from created team.
+    formatted_Prof_Data_List = []
+    
+    for i in range(len(self.team)):
+      formatted_Prof_Data = f"ID:{self.team_List[i]['id']}\nAccount ID:{self.team_List[i]['accountId']}\nPUUID:{self.team_List[i]['puuid']}\nProfileIconID:{self.team_List[i]['profileIconId']}\nRevision Data:{self.team_List[i]['revisionDate']}\nSummoner Level:{self.team_List[i]['summonerLevel']}"
+      formatted_Prof_Data_List.append(formatted_Prof_Data)
+    return formatted_Prof_Data_List
   
   def team_Lvl_Compare(self):
+    # Returns the summoner from the team with the highest level. 
     highest_Lvl = 0
     highest_Lvl_Name = ''
+    
     for i in range(len(self.team)):
       if self.team_List[i]['summonerLevel'] > highest_Lvl:
         highest_Lvl = self.team_List[i]['summonerLevel']
         highest_Lvl_Name = self.team_List[i]['name']
     return highest_Lvl_Name
 
-  def get_Match(self):
+  def get_Match(self, match_Quant):
+    # Returns a list of the last x matches for each summoner in the team.
     final_List = []
+    
     for i in range(len(self.team)):
       match_List = [] 
-      match_List.append(self.sum_Prof.match.matchlist_by_puuid(self.region, self.team_List[i]['puuid'], None, 5))
+      match_List.append(self.watcher.match.matchlist_by_puuid(self.region, self.team_List[i]['puuid'], None, match_Quant))
       final_List.append(match_List)
     return final_List
 
-  def team_Kills_Compare(self):
+  def team_Kills_Compare(self, match_Quant):
+    # Returns summoner in the team with the highest number of kills from the last x matches.
     target_Matches = []
     sum_Kills = []
     kills = 0
     kill_Leader = self.team[0]
+    kill_Loser = self.team[0]
     most_Kills = 0
-    leader_Prof = {'kl_Name': '', 'kl_Kills': 0}
+    least_Kills = 99999
+    av_Kills = 0
+    kill_Summary = {'kill_Leader': '', 'most_Kills': 0, 'kill_Loser': '', 'least_Kills': 0, 'av_Kills': 0}
     
-    for i in range(len(self.team)):
-      target_Matches.append(self.sum_Prof.match.matchlist_by_puuid(self.region, self.team_List[i]['puuid'], None, 3))
+    for i in range(len(self.team)): 
+      # Populates target matches with list of x matches for each summoner
+      target_Matches.append(self.watcher.match.matchlist_by_puuid(self.region, self.team_List[i]['puuid'], None, match_Quant))
     print(f"Target Matches:\n{target_Matches}")
     
     for i in range(len(target_Matches)):
+      # Traverses the target matches for each summoner
       print(f"Processing {self.team[i]}'s kills:")
       
       for j in range(len(target_Matches[i])):
+        # Creates an api call to fetch details for the ith match 
         print(f"{self.team[i]}'s kills for match {target_Matches[i][j]}:")
-        match_Results = self.sum_Prof.match.by_id(self.region, target_Matches[i][j])
+        match_Results = self.watcher.match.by_id(self.region, target_Matches[i][j])
         converted_Results = match_Results['info']['participants']
         
         for x in range(len(converted_Results)):
+          # Traverses participants in the match data to find a matching puuid from team list
           if converted_Results[x]['puuid'] == self.team_List[i]['puuid']:
+            # If puuid is recognized, adds kills to summoner's total kills
             print(converted_Results[x]['kills'])
             kills = kills + converted_Results[x]['kills']
             
-      sum_Kills.append(kills)
+      sum_Kills.append(kills) # Appends each players total kills to a list and resets kill count for next player
       kills = 0
       print(sum_Kills)
       
-      if sum_Kills[i] > most_Kills:
+      if sum_Kills[i] > most_Kills: # Determines which player has the most total kills
         most_Kills = sum_Kills[i]
         kill_Leader = self.team[i]
+      if sum_Kills[i] < least_Kills:
+        least_Kills = sum_Kills[i]
+        kill_Loser = self.team[i]
         
-    leader_Prof['kl_Name'] = kill_Leader
-    leader_Prof['kl_Kills'] = most_Kills
-    return leader_Prof
+    kill_Summary['kill_Leader'] = kill_Leader
+    kill_Summary['kill_Loser'] = kill_Loser
+    kill_Summary['most_Kills'] = most_Kills
+    kill_Summary['least_Kills'] = least_Kills
+    kill_Summary['av_Kills'] = sum(sum_Kills) / len(self.team)
+    
+    return kill_Summary # Returns a dict containing the player with highest kill's name and kill total
+
--- main.py
+++ main.py
@@ -2,27 +2,27 @@ from lolAPI import lol_Comp
 
 region = 'na1'
 sum_Team = []
-api_key = 'RGAPI-5cc939e3-30d1-49ee-b2e7-6456412a25f1'
+api_key = ''
 
-# sum_Team = ['someonesleftnut', 'hideonbush', 'Doublelift', 'trashley12345', 'flyingsquirrelly']
+team_Size = int(input("Please specify team size:"))
 
-team_Size = int(input("How many summoners will we be comparing?\n"))
-print(f"Please enter {team_Size} summoner names to form a team:\n")
+print(f"Please enter {team_Size} summoner names to form the team:\n")
 
-for i in range(team_Size):
+for i in range(0, team_Size):
   sum_Team.append(input(f'Summoner {i+1}:'))
+print('')
 
 player_Party_Profs = lol_Comp(api_key, region, sum_Team)
 player_Party_Profs.create_Team()
 
 for i in range(len(player_Party_Profs.team)):
-  print(f"{sum_Team[i]}'s info:\n{player_Party_Profs.get_Team_Info()[i]}\n")
-  
-print(f"{player_Party_Profs.team_Lvl_Compare()} is the highest level.\n")
+  print(f"{sum_Team[i]}'s Profile:\n{player_Party_Profs.get_Team_Info()[i]}\n")
+
 
 for i in range(len(player_Party_Profs.team)):
-  print(f"{player_Party_Profs.team[i]}'s match id(s):\n{player_Party_Profs.get_Match()[i]}\n")
+  print(f"{player_Party_Profs.team[i]}'s match id(s):\n{player_Party_Profs.get_Match(8)[i]}\n")
 
-player_Kill_Comp = player_Party_Profs.team_Kills_Compare()
+player_Kill_Comp = player_Party_Profs.team_Kills_Compare(4)
 
-print(f"{player_Kill_Comp['kl_Name']} achieved the most kills: {player_Kill_Comp['kl_Kills']}")
+print(f"{player_Kill_Comp['kill_Leader']} achieved the most kills: {player_Kill_Comp['most_Kills']}\n{player_Kill_Comp['kill_Loser']} achieved the least kills:{player_Kill_Comp['least_Kills']}\nAverage kills for all games is {player_Kill_Comp['av_Kills']}")
+
