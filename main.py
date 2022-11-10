519263c29bb7c2693b747799ed383c2e3ea7b863
Benjamin Fuller <benisfull@gmail.com>
2022-11-09 19:47:36 -0800

    reformat and add useful comments

--- main.py
+++ main.py
@@ -1,37 +1,26 @@
 from lolTeam import *
 from lolAPI import *
 
+# set default region to North America and input user API key (resets every 24 hours)
 region = 'na1'
 api_key = ''
 
-sum_Team = ['someonesleftnut', 'chiva11', 'flyingsquirrelly','trashley12345']
+sum_Team = ['someonesleftnut', 'chiva11', 'flyingsquirrelly', 'trashley12345']
+sum_Team2 = ['someonesleftnut', 'epilly', 'flyingsquirrelly', 'trashley12345']
+sum_Team3 = ['goawayannaokbye', 'chiva11', 'flyingsquirrelly', 'zaffreX']
+sum_Team4 = ['doublelift', 'chiva11', 'flyingsquirrelly', 'zaffreX']
+sum_Team5 = ['someonesleftnut', 'chiva11', 'flyingsquirrelly', 'trashley12345']
 
-sum_Team2 = ['someonesleftnut', 'epilly', 'flyingsquirrelly','trashley12345']
-
-sum_Team3 = ['goawayannaokbye', 'chiva11', 'flyingsquirrelly','zaffreX']
-
-sum_Team4 = ['doublelift', 'chiva11', 'flyingsquirrelly','zaffreX']
-
-sum_Team5 = ['someonesleftnut', 'chiva11', 'flyingsquirrelly','trashley12345']
-
-player_Party = lol_Team(api_key, region, sum_Team)
-
-player_Party2 = lol_Team(api_key, region, sum_Team2)
-
-player_Party3 = lol_Team(api_key, region, sum_Team3)
-
-player_Party4 = lol_Team(api_key, region, sum_Team4)                             
-
-player_Party5 = lol_Team(api_key, region, sum_Team5)
+player_Party = LolTeam(api_key, region, sum_Team)
+player_Party2 = LolTeam(api_key, region, sum_Team2)
+player_Party3 = LolTeam(api_key, region, sum_Team3)
+player_Party4 = LolTeam(api_key, region, sum_Team4)
+player_Party5 = LolTeam(api_key, region, sum_Team5)
 
 player_Party.create_Team()
-
 player_Party2.create_Team()
-
 player_Party3.create_Team()
-
 player_Party4.create_Team()
-
 player_Party5.create_Team()
 
 # for i in range(len(sum_Team)):
@@ -39,13 +28,15 @@ player_Party5.create_Team()
 
 # print(player_Party.match_Profiles)
 
-kill_Summary = team_Stat_Compare(player_Party, 'kills',5)
+kill_Summary = team_Stat_Compare(player_Party, 'kills', 5)
 
-print(f"{kill_Summary['stat_Leader']} achieved the most kills in your party: {kill_Summary['most_Stat']}\n{kill_Summary['stat_Loser']} achieved the least kills in your party: {kill_Summary['least_Stat']}\nAverage kills in the party was {kill_Summary['stat_Average']}\n")
+print(
+    f"{kill_Summary['stat_Leader']} achieved the most kills in your party: {kill_Summary['most_Stat']}\n{kill_Summary['stat_Loser']} achieved the least kills in your party: {kill_Summary['least_Stat']}\nAverage kills in the party was {kill_Summary['stat_Average']}\n")
 
 tank_Summary = team_Stat_Compare(player_Party, 'totalDamageTaken', 5)
 
-print(f"{tank_Summary['stat_Leader']} achieved the most damage tanked in your party: {tank_Summary['most_Stat']}\n{tank_Summary['stat_Loser']} achieved the least damage tanked in your party: {tank_Summary['least_Stat']}\nAverage damage tanked in the party was {kill_Summary['stat_Average']}\n")
+print(
+    f"{tank_Summary['stat_Leader']} achieved the most damage tanked in your party: {tank_Summary['most_Stat']}\n{tank_Summary['stat_Loser']} achieved the least damage tanked in your party: {tank_Summary['least_Stat']}\nAverage damage tanked in the party was {kill_Summary['stat_Average']}\n")
 
 # rank_Summary = team_Rank_Compare(player_Party)
 
